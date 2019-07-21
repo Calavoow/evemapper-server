b0d3779d491db962b72af6557ed248fa2722d6ac
Calavoow <Calavoow@users.noreply.github.com>
2019-07-21 12:30:23 -0400

    Update config of database and evesso

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,52 +1,15 @@
 const Sequelize = require('sequelize');
 const { Op } = Sequelize;
-const operatorsAliases = {
-  $eq: Op.eq,
-  $ne: Op.ne,
-  $gte: Op.gte,
-  $gt: Op.gt,
-  $lte: Op.lte,
-  $lt: Op.lt,
-  $not: Op.not,
-  $in: Op.in,
-  $notIn: Op.notIn,
-  $is: Op.is,
-  $like: Op.like,
-  $notLike: Op.notLike,
-  $iLike: Op.iLike,
-  $notILike: Op.notILike,
-  $regexp: Op.regexp,
-  $notRegexp: Op.notRegexp,
-  $iRegexp: Op.iRegexp,
-  $notIRegexp: Op.notIRegexp,
-  $between: Op.between,
-  $notBetween: Op.notBetween,
-  $overlap: Op.overlap,
-  $contains: Op.contains,
-  $contained: Op.contained,
-  $adjacent: Op.adjacent,
-  $strictLeft: Op.strictLeft,
-  $strictRight: Op.strictRight,
-  $noExtendRight: Op.noExtendRight,
-  $noExtendLeft: Op.noExtendLeft,
-  $and: Op.and,
-  $or: Op.or,
-  $any: Op.any,
-  $all: Op.all,
-  $values: Op.values,
-  $col: Op.col
-};
+const logger = require('./logger');
 
 module.exports = function (app) {
-  const connectionString = app.get('postgres');
-  const sequelize = new Sequelize(connectionString, {
-    dialect: 'postgres',
-    // logging: false,
-    operatorsAliases,
+  const db_config = app.get('database');
+  const sequelize = new Sequelize({...db_config, ...{
+    logging: false,
     define: {
       freezeTableName: true
     }
-  });
+  }});
   const oldSetup = app.setup;
 
   app.set('sequelizeClient', sequelize);
